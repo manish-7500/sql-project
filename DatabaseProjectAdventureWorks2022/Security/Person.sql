fb855d6bb40b72ebb9f95cb8ceffe7673d9dc86a
manish-7500 <manish.sharma88100@gmail.com>
2024-05-21 21:08:30 +0530

    first code

---
+++ DatabaseProjectAdventureWorks2022/Security/Person.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA [Person]
+    AUTHORIZATION [dbo];
+
+
+GO
+
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Contains objects related to names and addresses of customers, vendors, and employees', @level0type = N'SCHEMA', @level0name = N'Person';
+
+
+GO
+
---
+++ DatabaseProjectAdventureWorks2022/Security/Purchasing.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA [Purchasing]
+    AUTHORIZATION [dbo];
+
+
+GO
+
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Contains objects related to vendors and purchase orders.', @level0type = N'SCHEMA', @level0name = N'Purchasing';
+
+
+GO
+
